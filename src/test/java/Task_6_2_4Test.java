383a1f68d17fb6a08069dc01ae2ce0e4198c1cb6
Alyona275 <alena.kim.220795@gmail.com>
2023-02-24 10:54:58 +0900

    add Task 6.2.4

--- src/test/java/Task_6_2_4Test.java
+++ src/test/java/Task_6_2_4Test.java
@@ -42,59 +42,5 @@ public class Task_6_2_4Test {
         assertEquals(true, Task_6_2_4.Day.SATURDAY.isWeekend());
         assertEquals(true, Task_6_2_4.Day.SUNDAY.isWeekend());
     }
-
-//    private final PrintStream standardOut = System.out;
-//    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-//
-//    @BeforeEach
-//    public void setUp() {
-//        System.setOut(new PrintStream(outputStream));
-//    }
-//
-//    @AfterEach
-//    public void setStandardOut() {
-//        System.setOut(standardOut);
-//    }
-//
-//    @Test
-//    void catchCatTest() {
-//        Cat c = new Cat();
-//        Dog d = new Dog();
-//        d.catchCat(c);
-//        String str = outputStream.toString().trim();
-//        String[] lines = str.split("\\R");
-//        assertEquals("Cat caught", lines[0]);
-//        assertEquals("Woof!", lines[1]);
-//        assertEquals("Meow!", lines[2]);
-//    }
-//
-//    @Test
-//    void sayHelloCatTest() {
-//        Cat c = new Cat();
-//        c.sayHello();
-//        String str = outputStream.toString().trim();
-//        assertEquals("Meow!", str);
-//    }
-//
-//    @Test
-//    void sayHelloDogTest() {
-//        Dog d = new Dog();
-//        d.sayHello();
-//        String str = outputStream.toString().trim();
-//        assertEquals("Woof!", str);
-//    }
-//
-//    @Test
-//    void catchCatTestForFalseValue() {
-//        Cat c = new Cat();
-//        Dog d = new Dog();
-//        d.catchCat(c);
-//        String str = outputStream.toString().trim();
-//        String[] lines = str.split("\\R");
-//        assertNotEquals("Dog caught", lines[0]);
-//        assertNotEquals("", lines[1]);
-//        assertNotEquals("Wof!", lines[2]);
-//    }
-
 }
 
