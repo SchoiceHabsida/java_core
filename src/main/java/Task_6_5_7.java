00b89f573f1b44ea020a7a26a76ed443bfe7814c
Alyona275 <alena.kim.220795@gmail.com>
2023-03-06 18:57:01 +0900

    add Task 6.5.7

--- src/main/java/Task_6_5_7.java
+++ src/main/java/Task_6_5_7.java
@@ -2,28 +2,4 @@ import java.util.Arrays;
 
 public class Task_6_5_7 {
     //here your code
-    public static class AsciiCharSequence implements CharSequence {
-        private byte[] arrByte;
-
-        public AsciiCharSequence(byte[] arrByte) {
-            this.arrByte = arrByte;
-        }
-        @Override
-        public int length() {
-            return arrByte.length;
-        }
-        @Override
-        public char charAt(int index) {
-            return (char)arrByte[index];
-        }
-        @Override
-        public CharSequence subSequence(int start, int end) {
-            byte[] res = Arrays.copyOfRange(arrByte, start, end);
-            return new AsciiCharSequence(res);
-        }
-        @Override
-        public String toString() {
-            return new String(arrByte);
-        }
-    }
 }
