64e57df4f7f717d7055c533727fc97d66d6b4932
Kate <ekkim94@gmail.com>
2023-03-07 19:32:39 +0900

    add: Task 7.1.4

---
+++ src/main/java/Task_7_1_4.java
@@ -0,0 +1,5 @@
+public class Task_7_1_4 {
+    public static String getCallerClassAndMethodName() {
+        return null;
+    }
+}
---
+++ src/test/java/Task_7_1_4Test1.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task_7_1_4Test1 {
+
+    @Test
+    void testStackTrace() {
+        String result = callerMethod();
+        assertEquals("Task_7_1_4Test1#testStackTrace", result);
+    }
+
+    @Test
+    void testCorrectMethodNameDisplayed() {
+        String result = helperMethod();
+        assertEquals("Task_7_1_4Test1#helperMethod", result);
+    }
+
+    @Test
+    void testIncorrectMethodName() {
+        String result = callerMethod();
+        assertNotEquals("Task_7_1_4Test1#testCorrectMethodNameDisplayed", result);
+        assertEquals("Task_7_1_4Test1#testIncorrectMethodName", result);
+        assertNotEquals("jdk.internal.reflect.DirectMethodHandleAccessor#invoke", result);
+    }
+
+    @Test
+    void testNestedMethod() {
+        String result = helperMethod();
+        assertNotEquals("Task_7_1_4Test1#testCorrectMethodNameDisplayed", result);
+        assertEquals("Task_7_1_4Test1#helperMethod", result);
+        assertNotEquals("jdk.internal.reflect.DirectMethodHandleAccessor#invoke", result);
+    }
+
+    @Test
+    void testForNull() {
+        String result = Task_7_1_4.getCallerClassAndMethodName();
+        assertEquals("jdk.internal.reflect.DirectMethodHandleAccessor#invoke", result);
+    }
+
+    private String helperMethod() {
+        return callerMethod();
+    }
+
+    private String callerMethod() {
+        return Task_7_1_4.getCallerClassAndMethodName();
+    }
+}
