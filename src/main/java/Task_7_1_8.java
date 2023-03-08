5099b9e2a2ed17fde1851241be7ec467769203bc
Kate <ekkim94@gmail.com>
2023-03-08 15:13:46 +0900

    add: Task 7.1.8

---
+++ src/main/java/Task_7_1_8.java
@@ -0,0 +1,5 @@
+public class Task_7_1_8 {
+    public static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
+        // your solution goes here
+    }
+}
---
+++ src/test/java/Task_7_1_8Test.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task_7_1_8Test {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void setStandardOut() {
+        System.setOut(standardOut);
+    }
+
+    @RepeatedTest(3)
+    void testConnection() {
+        RobotConnectionManager robotConnectionManager = new RobotConnectionManagerImpl(3);
+        assertDoesNotThrow(() -> Task_7_1_8.moveRobot(robotConnectionManager, 1, 1));
+    }
+
+    @Test
+    void testRobotMovement() {
+        RobotConnectionManager robotConnectionManager = new RobotConnectionManagerImpl(2);
+        assertDoesNotThrow(() -> Task_7_1_8.moveRobot(robotConnectionManager, 1, 1));
+        String result = outputStream.toString().trim();
+        assertEquals("Robot was successfully moved" + System.getProperty("line.separator") + "Closing", result);
+    }
+
+    @Test
+    void testRobotFailedConnection() {
+        RobotConnectionManager robotConnectionManager = new RobotConnectionManagerImpl(4);
+        assertThrows(RobotConnectionException.class, () -> {
+            Task_7_1_8.moveRobot(robotConnectionManager, 1, 1);
+        });
+    }
+}
