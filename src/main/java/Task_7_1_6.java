76947f2bc30d9a510d7d6f20fa2642aaa76c6e86
Kate <ekkim94@gmail.com>
2023-03-13 17:43:33 +0900

    fix: test cases

--- src/main/java/Task_7_1_6.java
+++ src/main/java/Task_7_1_6.java
@@ -2,10 +2,19 @@ import java.util.Arrays;
 
 public class Task_7_1_6 {
     public static void main(String[] args) {
-
+        try (Car car = new Car()) {
+            car.drive();
+        } catch (java.lang.Exception e) {
+        }
     }
 
-    public static class Car {
-
+    public static class Car implements AutoCloseable {
+        public void drive() {
+            System.out.println("The car started driving.");
+        }
+        @Override
+        public void close() {
+            System.out.println("Shutting down the car...");
+        }
     }
 }
