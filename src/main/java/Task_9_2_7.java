de5a9dd96d426bf8475f66bf1c517a8aacb8768b
Alyona275 <alena.kim.220795@gmail.com>
2023-03-17 12:05:01 +0900

    add Task 9.2.8

--- src/main/java/Task_9_2_7.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-import java.io.Reader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-public class Task_9_2_7 {
-    //here your code
-}
---
+++ src/main/java/Task_9_2_8.java
@@ -0,0 +1,8 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Task_9_2_8 {
+    //here your code
+}
