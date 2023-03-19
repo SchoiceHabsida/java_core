cf8fb00c0af150a03f9013188597e145d540beba
Alyona275 <alena.kim.220795@gmail.com>
2023-03-20 08:28:42 +0900

    add Task 10.2.6

--- src/main/java/Task_10_2_5.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-public class Task_10_2_5 {
-    //here your code
-}
---
+++ src/main/java/Task_10_2_6.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Task_10_2_6 {
+    //here your code
+}
