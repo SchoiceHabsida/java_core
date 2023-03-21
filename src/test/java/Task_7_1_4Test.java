dce1384f4df6e990feeb9a0bb561e1431b4d670c
habsida <72781571+habsida@users.noreply.github.com>
2023-03-21 16:49:41 +0900

    Update Task_7_1_4Test.java

--- src/test/java/Task_7_1_4Test.java
+++ src/test/java/Task_7_1_4Test.java
@@ -7,28 +7,28 @@ class Task_7_1_4Test {
     @Test
     void testStackTrace() {
         String result = callerMethod();
-        assertEquals("Task_7_1_4Test1#testStackTrace", result);
+        assertEquals("Task_7_1_4Test#testStackTrace", result);
     }
 
     @Test
     void testCorrectMethodNameDisplayed() {
         String result = helperMethod();
-        assertEquals("Task_7_1_4Test1#helperMethod", result);
+        assertEquals("Task_7_1_4Test#helperMethod", result);
     }
 
     @Test
     void testIncorrectMethodName() {
         String result = callerMethod();
-        assertNotEquals("Task_7_1_4Test1#testCorrectMethodNameDisplayed", result);
-        assertEquals("Task_7_1_4Test1#testIncorrectMethodName", result);
+        assertNotEquals("Task_7_1_4Test#testCorrectMethodNameDisplayed", result);
+        assertEquals("Task_7_1_4Test#testIncorrectMethodName", result);
         assertNotEquals("jdk.internal.reflect.DirectMethodHandleAccessor#invoke", result);
     }
 
     @Test
     void testNestedMethod() {
         String result = helperMethod();
-        assertNotEquals("Task_7_1_4Test1#testCorrectMethodNameDisplayed", result);
-        assertEquals("Task_7_1_4Test1#helperMethod", result);
+        assertNotEquals("Task_7_1_4Test#testCorrectMethodNameDisplayed", result);
+        assertEquals("Task_7_1_4Test#helperMethod", result);
         assertNotEquals("jdk.internal.reflect.DirectMethodHandleAccessor#invoke", result);
     }
 
